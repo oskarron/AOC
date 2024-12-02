5626ca6940960ab8354764004c2834fc0765f2ec
oskarron <oskar.ronningstad@gmail.com>
2024-12-02 09:04:15 +0100

    part 2

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("part1: " + day1.part1());
+        System.out.println("part2: " + day1.part2());
+    }
+}
--- src/day1.java
+++ src/day1.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.*;
 
 public class day1 {
-    public static void main(String[] args) {
+    public static int part1(){
         try {
 
             File file = new File("inputs/day1");
@@ -20,19 +20,64 @@ public class day1 {
                 secondNumber.add(Integer.parseInt(splitLine[3]));
 
             }
+
             Collections.sort(firstNumber);
             Collections.sort(secondNumber);
             int difference = 0;
+
             for (int i = 0; i < firstNumber.size(); i++) {
                 int dif = firstNumber.get(i) - secondNumber.get(i);
                 difference += Math.abs(dif);
-
             }
-            System.out.println(difference);
 
             input.close();
+            return difference;
         } catch (Exception ex) {
             ex.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static int part2(){
+        try {
+
+            File file = new File("inputs/day1");
+
+            Scanner input = new Scanner(file);
+
+            Set<Integer> firstNumber = new HashSet<>();
+            HashMap<Integer, Integer> secondNumber = new HashMap<>();
+
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+                String[] splitLine = line.split(" ");
+
+                firstNumber.add(Integer.parseInt(splitLine[0]));
+
+                if(secondNumber.containsKey(Integer.parseInt(splitLine[3]))){
+                    int antall = secondNumber.get(Integer.parseInt(splitLine[3]));
+                    antall +=1;
+                    secondNumber.put(Integer.parseInt(splitLine[3]), antall);
+
+                } else{
+                    secondNumber.put(Integer.parseInt(splitLine[3]), 1);
+                }
+
+            }
+            int similarityScore = 0;
+
+            for (int number: firstNumber){
+                if(secondNumber.containsKey(number)){
+                    similarityScore += number * secondNumber.get(number);
+                }
+            }
+
+
+            input.close();
+            return similarityScore;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return 0;
         }
     }
 }
