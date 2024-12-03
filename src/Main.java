51dd739bb90e5328b7e12ea309bf1f7e76e51a30
oskarron <oskar.ronningstad@gmail.com>
2024-12-03 19:57:40 +0100

    dag 2 part 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        ExecutionTimer.measureExecutionTime("part 1: ", day1::part1);
-        ExecutionTimer.measureExecutionTime("part 2: ", day1::part2);
+        //ExecutionTimer.measureExecutionTime("part 1: ", day1::part1);
+        //ExecutionTimer.measureExecutionTime("part 2: ", day1::part2);
+        ExecutionTimer.measureExecutionTime("day 2, part 1: ", day2::part1);
+        System.out.println(day2.part1());
+        ExecutionTimer.measureExecutionTime("day 2, part 2: ", day2::part2);
+        System.out.println(day2.part2());
     }
 }
--- src/day2.java
+++ src/day2.java
@@ -1,53 +1,83 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class day2 {
-    public static int part1(){
+    public static int part1() {
 
         try {
             File file = new File("inputs/day2");
             Scanner input = new Scanner(file);
             int safeReports = 0;
-            int line = 0;
             while (input.hasNextLine()) {
-
-                boolean safe = true;
                 String[] splitInput = input.nextLine().split(" ");
-                int a = Integer.parseInt(splitInput[0]);
-                int b = Integer.parseInt(splitInput[1]);
-                boolean ascending = a - b <= 0;
-
-                for (int i = 0; i < splitInput.length-1; i++) {
-                    int firstNumber = Integer.parseInt(splitInput[i]);
-                    int secondNumber = Integer.parseInt(splitInput[i+1]);
-                    int absoluteDifference = Math.abs(firstNumber - secondNumber);
-
-                    //sjekk om ascending / descending
-                    boolean checkAscending = firstNumber - secondNumber <= 0;
-                    if(checkAscending != ascending){
-                        safe = false;
-                        break;
-                    }
-                    //sjekke differanse
-                    if((absoluteDifference == 0 || absoluteDifference > 3)) {
-                        safe = false;
-                        break;
-                    }
-
-                }
-                line+=1;
-                if (safe) {
-                    safeReports += 1;
+                if (safeReport(splitInput)) {
+                    safeReports++;
                 }
             }
-            input.close();
-            System.out.println(safeReports);
+            return safeReports;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return 0;
+    }
+
+    public static int part2() {
+        try {
+            File file = new File("inputs/day2");
+            Scanner input = new Scanner(file);
+            int safeReports = 0;
+
+            while (input.hasNextLine()) {
+                String[] splitInput = input.nextLine().split(" ");
+                if (safeReport(splitInput)) {
+                    safeReports++;
+                } else {
+                    for (int i = 0; i < splitInput.length; i++) {
+                        String[] splitInputCopy = new String[splitInput.length - 1];
+                        for (int j = 0, k = 0; j < splitInput.length; j++) {
+                            if (i != j) {
+                                splitInputCopy[k++] = splitInput[j];
+                            }
+                        }
+                        if (safeReport(splitInputCopy)) {
+                            safeReports++;
+                            break;
+                        }
+                    }
+                }
+            }
             return safeReports;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return 0;
+    }
+
+    public static boolean safeReport(String[] line) {
+        boolean safe = true;
+        int a = Integer.parseInt(line[0]);
+        int b = Integer.parseInt(line[1]);
+        boolean ascending = a - b <= 0;
+
+        for (int i = 0; i < line.length - 1; i++) {
+            int firstNumber = Integer.parseInt(line[i]);
+            int secondNumber = Integer.parseInt(line[i + 1]);
+            int absoluteDifference = Math.abs(firstNumber - secondNumber);
+
+            //sjekk om ascending / descending
+            boolean checkAscending = firstNumber - secondNumber <= 0;
+            if (checkAscending != ascending) {
+                safe = false;
+                break;
+            }
+            //sjekke differanse
+            if ((absoluteDifference == 0 || absoluteDifference > 3)) {
+                safe = false;
+                break;
+            }
+
+        }
+        return safe;
     }
 }
