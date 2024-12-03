9af5bf96e8d3ceb95eecd932d71992f3ee33c332
oskarron <oskar.ronningstad@gmail.com>
2024-12-03 16:13:14 +0100

    hei theodor

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         ExecutionTimer.measureExecutionTime("part 1: ", day1::part1);
         ExecutionTimer.measureExecutionTime("part 2: ", day1::part2);
+        ExecutionTimer.measureExecutionTime("day 2 part1: ", day2::part1);
     }
 }
