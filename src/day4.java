0968b418ed3805d14a1026de694067e22fa05e9d
oskarron <oskar.ronningstad@gmail.com>
2024-12-04 14:33:47 +0100

    dag 4 part 2

--- src/day4.java
+++ src/day4.java
@@ -22,13 +22,42 @@ public class day4 {
         for (String line : stringList) {
             for (int col = 0; col < line.length(); col++) {
                 if (line.charAt(col) == 'X') {
-                    xmasAppearances+=checkDirections(row, col);
+                    xmasAppearances += checkDirections(row, col);
                 }
             }
             row++;
         }
         return xmasAppearances;
     }
+
+    public static int part2() {
+        int xShapeAppearances = 0;
+        listOfLines();
+
+        for (int row = 1; row < stringList.size() - 1; row++) {
+            for (int col = 1; col < stringList.get(row).length() - 1; col++) {
+                if (stringList.get(row).charAt(col) == 'A') {
+                    if (checkXShape(row, col)) {
+                        xShapeAppearances++;
+                    }
+                }
+            }
+        }
+        return xShapeAppearances;
+    }
+
+    public static boolean checkXShape(int row, int col) {
+        char upperLeft = stringList.get(row - 1).charAt(col - 1);
+        char upperRight = stringList.get(row - 1).charAt(col + 1);
+        char lowerLeft = stringList.get(row + 1).charAt(col - 1);
+        char lowerRight = stringList.get(row + 1).charAt(col + 1);
+
+        if (((upperLeft == 'S' && lowerRight == 'M') || (upperLeft == 'M' && lowerRight == 'S'))) {
+            return (upperRight == 'S' && lowerLeft == 'M') || (upperRight == 'M' && lowerLeft == 'S');
+        }
+        return false;
+    }
+
     public static void listOfLines() {
         ArrayList<String> list = new ArrayList<>();
         try {
@@ -51,13 +80,13 @@ public class day4 {
         for (int[] offset : directionalOffsets) {
             boolean isXmas = true;
             for (int i = 1; i < 4; i++) {
-                int newRow = row + (offset[0]*i);
-                int newCol = col + (offset[1]*i);
+                int newRow = row + (offset[0] * i);
+                int newCol = col + (offset[1] * i);
                 // kræsjer i kanten
                 if (!isLegalIndex(newRow, newCol)) {
                     isXmas = false;
                     break;
-                //feil bokstav
+                    //feil bokstav
                 } else if (xmasMap.get(i) != stringList.get(newRow).charAt(newCol)) {
                     isXmas = false;
                     break;
